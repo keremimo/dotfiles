9f6b2da0168adfc525fd1884eb8399eb08fc6ec9
Kerem Kilic <git@keremk.be>
2026-05-21 21:49:10 +0200

    Update in general

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -9,3 +9,14 @@ alias cd=z
 alias ls=eza
 
 export EDITOR=nvim
+
+# Default graphical applications to the Intel iGPU on this hybrid system.
+set -gx __EGL_VENDOR_LIBRARY_FILENAMES /usr/share/glvnd/egl_vendor.d/50_mesa.json
+set -gx __GLX_VENDOR_LIBRARY_NAME mesa
+set -gx VK_DRIVER_FILES /usr/share/vulkan/icd.d/intel_icd.json
+set -gx VK_ICD_FILENAMES /usr/share/vulkan/icd.d/intel_icd.json
+set -gx MESA_VK_DEVICE_SELECT 8086:a788
+set -gx DRI_PRIME 0
+set -gx __NV_PRIME_RENDER_OFFLOAD 0
+set -gx WGPU_BACKEND vulkan
+set -gx WGPU_POWER_PREF low
